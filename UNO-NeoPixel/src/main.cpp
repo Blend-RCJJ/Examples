c6eefcd50171360fcfe4603abc9d75c1ffaf454f
onagi37s <suminagiotibi@icloud.com>
2022-11-07 22:49:30 +0900

    クリスマス

--- UNO-NeoPixel/src/main.cpp
+++ UNO-NeoPixel/src/main.cpp
@@ -1,8 +1,8 @@
-#include <Arduino.h>
 #include <Adafruit_NeoPixel.h>
+#include <Arduino.h>
 
-const int pin = 6;       //ピン番号
-const int numOfLED = 8;  // LEDの数
+const int pin      = 6;   //ピン番号
+const int numOfLED = 32;  // LEDの数
 
 Adafruit_NeoPixel led(numOfLED, pin,
                       NEO_GRB + NEO_KHZ800);  // LED(numOfLED)個を6番ピンに設定
@@ -14,9 +14,46 @@ void setup(void) {
 void loop(void) {
     led.begin();  // 始まりの合図
 
-    led.setBrightness(50);                      //明るさの定義
-    led.setPixelColor(0 /*0番目*/, 255, 100, 0);  // 1番目のLEDを赤色に
-    led.setPixelColor(1/*0番目*/, 255, 0, 255);  // 1番目のLEDを赤色に
+    led.setBrightness(50);  //明るさの定義
+
+    for (int i = 0; i < numOfLED; i++) {
+        if (i % 3 == 0) {
+            
+            led.show();
+            led.setPixelColor(i, 0, 0, 0);
+            delay(10);
+
+        } else if (i % 3 == 1) {
+           
+            led.show();
+            led.setPixelColor(i, 0, 0, 0);
+            delay(20);
+
+        } else {
+            
+            led.show();
+            led.setPixelColor(i, 0, 0, 0);
+            delay(30);
+        }
+    }
+
+    for (int i = 0; i < numOfLED; i++) {
+        if (i % 3 == 0) {
+            delay(100);
+            led.setPixelColor(i, 255, 0, 0);
+            led.show();
+        } else if (i % 3 == 1) {
+            delay(150);
+            led.setPixelColor(i, 0, 255, 0);
+            led.show();
+        } else {
+            delay(200);
+            led.setPixelColor(i, 100, 100, 100);
+            led.show();
+        }
+    }
 
     led.show();  // LEDに反映させる
 }
+
+//(red,green,blue)
