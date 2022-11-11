c5b01eb65ac83371fcb5b2f3838cb98e4138d805
onagi37s <suminagiotibi@icloud.com>
2022-11-11 21:50:49 +0900

    sin(LED)

--- bouncing/src/main.cpp
+++ bouncing/src/main.cpp
@@ -1,30 +1,14 @@
 #include <Arduino.h>
 
-const int LED    = 13;
-const int BUTTON = 7;
-
-int val     = 0;
-int old_val = 0;
-int state   = 0;
+const int LED = 9;
+int count     = 0;
 
 void setup() {
     pinMode(LED, OUTPUT);
-    pinMode(BUTTON, INPUT);
 }
 
 void loop() {
-    val = digitalRead(BUTTON);
-
-    //変化があるかの確認
-    if ((val == HIGH) && (old_val == LOW)) {
-        state = 1 - state;
-        delay(10);  //バウンシング対策
-    }
-
-    old_val = val;
-    if (state == 1) {
-        digitalWrite(LED, HIGH);
-    } else {
-        digitalWrite(LED, LOW);
-    }
+    analogWrite(LED, 127*sin(count/50.0)+127); //sin(count)=-1の時LEDの明るさが0
+    delay(10); //変化を見るための待機
+    count++;
 }
