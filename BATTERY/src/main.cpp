eee2fdeff9ac5b269c3d72f3d21769aeed1fc940
onagi37s <suminagiotibi@icloud.com>
2022-11-20 23:22:00 +0900

    色の微調整をしました。

--- BATTERY/src/main.cpp
+++ BATTERY/src/main.cpp
@@ -6,7 +6,7 @@ const int numberOfLED = 24;
 
 Adafruit_NeoPixel led(numberOfLED,LED_PINS,NEO_GRB + NEO_KHZ800);
 
-void hoge(int voltage){
+void battery(int voltage){
   led.begin();
   led.setBrightness(50);
   for (int i = 0; i < 24; i++)
@@ -22,46 +22,46 @@ void hoge(int voltage){
   if(i <=10){
     led.setPixelColor(0,255,0,0);
     led.setPixelColor(1,255,0,0);
-    // led.show();
+  
   }else if(i <=20){
     led.setPixelColor(2,200,55,0);
     led.setPixelColor(3,200,55,0);
-    // led.show();
+
   }else if(i <=30){
     led.setPixelColor(4,190,65,0);
     led.setPixelColor(5,180,75,0);
     led.setPixelColor(6,180,75,0);
-    // led.show();
+   
   }else if(i <=40){
     led.setPixelColor(7,170,85,0);
     led.setPixelColor(8,160,95,0);
     led.setPixelColor(9,160,95,0);
-    // led.show();
+ 
   }else if(i <=50){
     led.setPixelColor(10,140,115,0);
     led.setPixelColor(11,120,125,0);
     led.setPixelColor(12,120,125,0);
-    // led.show();
+    
   }else if(i <=60){
     led.setPixelColor(13,100,120,35);
     led.setPixelColor(14,90,110,55);
-    // led.show();
+    
   }else if(i <=70){
     led.setPixelColor(15,80,100,75);
     led.setPixelColor(16,70,90,95);
-    // led.show();
+    
   }else if(i <=80){
     led.setPixelColor(17,60,80,115);
     led.setPixelColor(18,50,70,135);
-    // led.show();
+    
   }else if(i <=90){
     led.setPixelColor(19,40,60,155);
     led.setPixelColor(20,30,50,175);
-    // led.show();
+    
   }else if(i <=99){
     led.setPixelColor(21,20,40,195);
-    led.setPixelColor(22,10,30,215);
-    // led.show();
+    led.setPixelColor(22,10,35,210);
+   
   }else if(i <=1000){
     led.setPixelColor(23,0,0,255);
 
@@ -78,7 +78,7 @@ void setup() {
 void loop() {
   for (int i = 0; i <= 100; i++)
   {
-    hoge(i);
+    battery(i);
     delay(200);
   }
   
